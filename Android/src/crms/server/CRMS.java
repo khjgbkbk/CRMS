ee83fe660b1b1a0e311a7701bed068f887e2747f
PichuChen <pichubaby@gmail.com>
2011-05-22 01:17:38 +0800

    Start Android

---
+++ Android/src/crms/server/CRMS.java
@@ -0,0 +1,13 @@
+package crms.server;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CRMS extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
