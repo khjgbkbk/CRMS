e4664532b121843275a1216c8972966102ac31aa
mkfsn <p408865@hotmail.com>
2011-05-22 01:16:41 +0900

    delete http to post only

--- qb/login.php
+++ qb/login.php
@@ -1,40 +1,29 @@
 <?php
-	//header('WWW-Authenticate: Basic realm="My Realm"');
-	echo $_SERVER['PHP_AUTH_USER']." : ".$_SERVER['PHP_AUTH_PW'];
-	exit;
-	if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']))
-	{
-		//header('WWW-Authenticate: Basic realm="My Realm"');
-		//header('HTTP/1.1 401 Unauthorized');
-		echo "error";
-	}
-	else if($_SERVER['PHP_AUTH_USER']=="") 
+	if(!isset($_POST['UsrID']) || $_POST['UsrID']=="") 
 	{
 		echo "nid";
 	} 
-	else if($_SERVER['PHP_AUTH_PW']=="")
+	else if(!isset($_POST['UsrPW']) || $_POST['UsrPW']=="")
 	{
 		echo "npd";
 	}
-	else if ($_SERVER['PHP_AUTH_USER']=="mkfsn" && $_SERVER['PHP_AUTH_PW']=="kanade")
-	{
-		header('HTTP/1.1 200 ok');
-		$username = $_SERVER['PHP_AUTH_USER'];
-		$password = $_SERVER['PHP_AUTH_PW'];
-		unset($_SERVER['PHP_AUTH_USER']);
-		unset($_SERVER['PHP_AUTH_PW']);
-		echo '<form method="post" name="kakusu" action="main.php">';
-		echo '<input type="hidden" name="Uid" value="'.$username.'">';
-		echo '<input type="hidden" name="Upd" value="'.$password.'">';
-		echo '</form>';
-		echo '<script language="JavaScript">';
-		echo 'document.kakusu.submit();';
-		echo '</script>';
-	}
 	else
 	{
-		header('WWW-Authenticate: Basic realm="My Realm"');
-		header('HTTP/1.1 401 Unauthorized');
-		echo 'error';
+		include("../database/fLogin.php");
+		$return = funcLogin(array($_POST['UsrID'], $_POST['UsrPW']));
+		if($return['success'])
+		{
+			echo '<form method="post" name="kakusu" action="main.php">';
+			echo '<input type="hidden" name="Uid" value="'.$_POST['UsrID'].'">';
+			echo '<input type="hidden" name="Upd" value="'.$_POST['UsrPW'].'">';
+			echo '</form>';
+			echo '<script language="JavaScript">';
+			echo 'document.kakusu.submit();';
+			echo '</script>';
+		}
+		else
+		{
+			echo "error";
+		}
 	}
 ?>
