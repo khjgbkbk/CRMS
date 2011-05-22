e497edd7cdf43a3353d61c1be4c676bbb046e47e
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-05-22 22:31:58 +0800

    new

---
+++ database/fQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+	function funcQuery($ask){
+		include("mysql_connect.php");
+
+		if( isset($ask['id']) && $ask['id']!="" ){
+			$sql = "select * from ".$db_equip." where id = '{$ask['id']}'";
+			$result = mysql_query($sql) or die(mysql_error());
+			$row = mysql_fetch_row($result);
+			if( $row[0]=="" ){
+				return array("success" => false);
+			}else{
+				$result = $row;
+				return array("success" => true, "data" => $result);
+			}
+		}else{
+			return array("success" => false);
+		}
+	}
+?>
---
+++ database/fRegister.php
@@ -0,0 +1,24 @@
+<?php
+	funcRegister($ask){
+		include("mysql_connect.php");
+
+		if( isset($ask['username']) && isset($ask['password']) && $ask['username']!="" && $ask['password']!="" ){
+			$sql = "select * from ".$db_shadow." where name = '{$ask['username']}'";
+			$result = mysql_query($sql) or die(mysql_error());
+			$row = mysql_fetch_row($result);
+
+			if($row[1]!=""){
+				return array("success" => false); 
+			}else{
+				$sql = "insert into ".$db_shadow." (name, passwd) values ('{$ask['username']}', '{$ask['password']}')";
+				if( mysql_query($sql) or die(mysql_error()) ){
+					return array("success" => true);
+				}else{
+					return array("success" => false);
+				}
+			}
+		}else{
+			return array("success" => false);
+		}
+	}
+?>
