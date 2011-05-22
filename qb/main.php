2a4128e1208dff457c60a046283459d501f179f2
mkfsn <p408865@hotmail.com>
2011-05-22 17:26:40 +0900

    html to php

--- qb/main.php
+++ qb/main.php
@@ -8,16 +8,21 @@
 </head>
 <body>
 <?php
-	if(isset($_SESSION["loginid"]) && isset($_SESSION["loginpwd"]))
-	{
-		echo "<div align='center'>";
-		echo "<h1>Hello ".htmlspecialchars($_SESSION["loginid"])."</h1>";
-		echo "Your password is ".htmlspecialchars($_SESSION["loginpwd"])."<br/>";
-		echo "Correct?<br/>";
-		echo "Unfortunately, this Website is still building...<br/>";
-		echo "<a href='./logout.php'>Log out</a>";
-		echo "</div>";
-	}
+if(isset($_SESSION["loginid"]) && isset($_SESSION["loginpwd"]))
+{
+	echo "<div align='center'>";
+	echo "<h1>Hello ".htmlspecialchars($_SESSION["loginid"])."</h1>";
+	echo "Your password is ".htmlspecialchars($_SESSION["loginpwd"])."<br/>";
+	echo "Correct?<br/>";
+	echo "Unfortunately, this Website is still building...<br/>";
+	echo "<a href='./logout.php'>Log out</a>";
+	echo "</div>";
+}
+else
+{
+	header('Location: ./');
+	exit;
+}
 ?>
 </body>
 </html>
