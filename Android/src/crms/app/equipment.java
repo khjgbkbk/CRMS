8e8a4bb84f0ff71c5a3a512baf999bc2d5b4820f
PichuChen <pichubaby@gmail.com>
2011-05-23 15:14:25 +0800

    compete equipment

--- Android/src/crms/app/equipment.java
+++ Android/src/crms/app/equipment.java
@@ -18,14 +18,34 @@ public class equipment {
 	public String toString(){
 		return _name;
 	}
-	public location getLocation(){
+	public location location(){
 		return _Location;
 	}
-
-	public equipment setLocation(location L){
+	public equipment location(location L){
 		_Location = L;
 		return this;
 	}
+	
+	public String id(){return _id;}
+	public equipment id(String s){_id = s;return this;}
+	
+	public String name(){return _name;}
+	public equipment name(String s){_name=s;return this;}
+	
+	public int price(){return _price;}
+	public equipment price(int s){_price=s;return this;}
+	
+	
+	public Date date(){return _date;}
+	public equipment date(String s) throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		_date= sdf.parse(s);
+		return this;
+	}
+	public equipment date(Date d){_date=d;return this;}
+	
+	
+	
 	
 	public equipment(JSONObject jsonObject) throws JSONException, ParseException {
 		// TODO Auto-generated constructor stub
