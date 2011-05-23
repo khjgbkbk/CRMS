dc5f551bbaf5487f056c3bea93f4dc9e119abefb
PichuChen <pichubaby@gmail.com>
2011-05-23 11:55:03 +0800

    build Query equipment(Android)

--- Android/src/crms/app/equipment.java
+++ Android/src/crms/app/equipment.java
@@ -1,5 +1,31 @@
 package crms.app;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class equipment {
+	String _id;
+	String _name;
+	location _Location = null;
+	int _price;
+	Date _date;
+	
+	public equipment(JSONObject jsonObject) throws JSONException, ParseException {
+		// TODO Auto-generated constructor stub
+		//try{
+		_id = jsonObject.getString("id");
+		_name = jsonObject.getString("name");
+		_Location = new location(jsonObject.getString("dorm"));
+		String dateString = jsonObject.getString("date");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		//¶i¦æÂà´«
+		_date = sdf.parse(dateString);
+	}
 
 }
