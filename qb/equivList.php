97446fe60530376a4d4dbce58b3d4a485eaca6c9
mkfsn <p408865@hotmail.com>
2011-06-09 01:37:06 +0900

    把刪除使用者的頁面刪除掉 預計把他放在使用者列表的其中一個欄位

--- qb/equivList.php
+++ qb/equivList.php
@@ -1,89 +1,7 @@
 <?php
-	ob_start();
-	session_start();
-?>
-<!-- function send -->
-<script type="text/javascript">
-	function send()
-	{
-		if( $("input[name='Equivname']").attr('value') == "" )
-		{
-			$('div.message').html('請輸入器材名稱!');
-			return;
-		}
-		if( $("input[name='Equivplace']").attr('value') == "" )
-		{
-			$('div.message').html('請輸入器材所在位置!');
-			return;
-		}
-		if( $("input[name='Equivid']").attr('value') == "" )
-		{
-			$('div.message').html('請輸入器材的編號!');
-			return;
-		}
-		$.ajax({
-			url: 'addequivto.php',
-			type: 'POST',
-			data: {
-				addEquivName: 	$("input[name='Equivname']").attr('value'),
-				addEquivPlace: 	$("input[name='Equivplace']").attr('value'),
-				addEquivId: 	$("input[name='Equivid']").attr('value'),
-				addEquivPrice: 	$("input[name='Equivprice']").attr('value'),
-			},
-			dataType: "json",
-			error: function(xhr) {
-				alert('Ajax request failure');
-			},
-			success: function(result) {
-				switch (result) {
-				case "noname":
-					$('div.message').html("Please enter the Name");
-					break;
-				case "noplace":
-					$('div.message').html("Please enter the Place !!");
-					break;
-				case "noid":
-					$('div.message').html("Please enter the ID !!");
-					break;
-				case "success":
-					alert('Successed !!');
-					document.location.href = "./";
-					break;
-				case "fail":
-					alert('Failed !!');
-					$('input[name="Equivname"]').attr({value:''}); 
-					$('input[name="Equivplace"]').attr({value:''}); 
-					$('input[name="Equivid"]').attr({value:''});
-					$('input[name="Equivprice"]').attr({value:''});
-					break;
-				default:
-					$('div.message').html(result);
-					break;
-				}
-			},
-		});
-	}
-</script>
-<!-- click action -->
-<script type="text/javascript">
-	var KEY_ENTER = 13;
-	$(document).ready(function () 
-	{
-		$('#Send').click(function()
-		{
-			send();
-		});
-	})
-	$(document).keydown(function(event){ 
-		//如果按 enter
-		if(event.keyCode == KEY_ENTER)
-		{
-			send();
-		}
-	});
-</script>
+ob_start();
+session_start();
 
-<?php
 if(isset($_SESSION["loginid"]) && isset($_SESSION["loginpwd"]))
 {
 	include("../database/fList.php");
@@ -103,6 +21,7 @@ if(isset($_SESSION["loginid"]) && isset($_SESSION["loginpwd"]))
 			<td></td>
 			<td>器材價錢</td>
 			<td>器材編號</td>
+			<td>刪除</td>
 		</tr>
 <?php
 		for($i=0 ; $i<$row ; $i++)
@@ -119,6 +38,7 @@ if(isset($_SESSION["loginid"]) && isset($_SESSION["loginpwd"]))
 <?php
 			}
 ?>
+		<td><input type="button" value="刪除"><td>
 	</tr>
 <?php
 		}
