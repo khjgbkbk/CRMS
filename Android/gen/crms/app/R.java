379b04dea6dfe628bd08580380ca962a09e68268
gary0431 <gary0431@gmail.com>
2011-06-09 02:45:59 +0800

    layout edit

--- Android/gen/crms/app/R.java
+++ Android/gen/crms/app/R.java
@@ -45,9 +45,10 @@ public final class R {
         public static final int edit=0x7f030000;
         public static final int main=0x7f030001;
         public static final int menu=0x7f030002;
-        public static final int newitem=0x7f030003;
-        public static final int query=0x7f030004;
-        public static final int remove=0x7f030005;
+        public static final int newid=0x7f030003;
+        public static final int newitem=0x7f030004;
+        public static final int query=0x7f030005;
+        public static final int remove=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
