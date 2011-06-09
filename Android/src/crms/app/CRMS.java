d95e2adce114f1dc193639b109190cef4b9aea79
gary0431 <gary0431@gmail.com>
2011-06-09 22:03:15 +0800

    rmBack change

--- Android/src/crms/app/CRMS.java
+++ Android/src/crms/app/CRMS.java
@@ -135,7 +135,7 @@ public class CRMS extends Activity {
     public void goRm(View cvView){
     	setContentView(R.layout.remove);
     }
-    public void rmback(View cvView){
+    public void rmBack(View cvView){
     	setContentView(R.layout.menu);
     }
     public void gotomain(View cvView){
