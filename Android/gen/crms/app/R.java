88efd005f60dff7cc4e8de3856a3ce0a2091ed88
gary0431 <gary0431@gmail.com>
2011-06-09 20:46:22 +0800

    Sign fix button

--- Android/gen/crms/app/R.java
+++ Android/gen/crms/app/R.java
@@ -22,9 +22,9 @@ public final class R {
         public static final int button3=0x7f05000c;
         public static final int button4=0x7f05000d;
         public static final int editText1=0x7f050010;
-        public static final int editText2=0x7f050014;
+        public static final int editText2=0x7f050011;
         public static final int editText3=0x7f050016;
-        public static final int editText4=0x7f050013;
+        public static final int editText4=0x7f050014;
         public static final int linearLayout1=0x7f050008;
         public static final int newitemdorm=0x7f05001c;
         public static final int newitemeqid=0x7f050019;
@@ -40,14 +40,14 @@ public final class R {
         public static final int tableRow2=0x7f050005;
         public static final int tableRow3=0x7f050009;
         public static final int tableRow4=0x7f050015;
-        public static final int tableRow5=0x7f050011;
+        public static final int tableRow5=0x7f050012;
         public static final int tableRow6=0x7f050017;
         public static final int tableRow9=0x7f05001d;
         public static final int textView1=0x7f050003;
         public static final int textView2=0x7f05000f;
         public static final int textView3=0x7f050006;
         public static final int textView4=0x7f050001;
-        public static final int textView5=0x7f050012;
+        public static final int textView5=0x7f050013;
         public static final int uid=0x7f050004;
     }
     public static final class layout {
