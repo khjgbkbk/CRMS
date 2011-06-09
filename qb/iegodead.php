0871d13354e54c2b56db0919719bf18eb0766aa4
mkfsn <p408865@hotmail.com>
2011-06-10 04:18:21 +0900

    刪除不重要的檔案

--- qb/iegodead.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<title>CRMS</title>
-<link rel="shortcut icon" href="image/qb_ico.gif">
-</head>
-<body bgcolor="#EBF5FF"  link="1C19FF" vlink="1C19FF">
-</body>
-</html>
