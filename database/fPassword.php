a439804fc815c75a3ee0d61255bc6c4a36514b73
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 15:55:03 +0800

    variable name corrected

--- database/fPassword.php
+++ database/fPassword.php
@@ -2,19 +2,19 @@
 	function funcPassword($ask){
 		include("mysql_connect.php");
 
-		if( isset($ask['name']) && isset($ask['passwd']) && $ask['name']!="" && $ask['passwd']!="" ){
-			$sql = "select * from ".$db_shadow." where name = '{$ask['name']}'";
+		if( isset($ask['username']) && isset($ask['password']) && $ask['username']!="" && $ask['password']!="" ){
+			$sql = "select * from ".$db_shadow." where name = '{$ask['username']}'";
 			$result = mysql_query($sql) or die(mysql_error());
 			$row = mysql_fetch_row($result);
 
 			if($row[0]==""){
-				return array("success" => false, "status" => "name not found");
+				return array("success" => false, "status" => "username not found");
 			}else{
-				$sql = "update ".$db_shadow." set passwd='{$ask['passwd']}' where name = '{$ask['name']}'";
+				$sql = "update ".$db_shadow." set passwd='{$ask['password']}' where name = '{$ask['username']}'";
 				if( mysql_query($sql) or die(mysql_error()) ){
 					return array("success" => true);
 				}else{
-					return array("success" => false, "status" => "failed to change passwd");
+					return array("success" => false, "status" => "failed to change password");
 				}
 			}
 		}else{
