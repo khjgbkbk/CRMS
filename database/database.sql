b2e8e5aa606b44119e1e68db0cf2294e24429199
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 16:46:41 +0800

    new buildings table

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 08, 2011 at 11:31 PM
+-- Generation Time: Jun 10, 2011 at 04:45 PM
 -- Server version: 5.1.54
 -- PHP Version: 5.3.5-1ubuntu7.2
 
@@ -22,46 +22,31 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `equipment`
+-- Table structure for table `buildings`
 --
 
-CREATE TABLE IF NOT EXISTS `equipment` (
-  `name` varchar(40) NOT NULL,
-  `dorm` varchar(40) NOT NULL,
-  `id` varchar(40) NOT NULL,
-  `price` int(11) NOT NULL,
-  `date` date NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `buildings` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `building` varchar(15) NOT NULL,
+  `desc` int(15) NOT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
 --
--- Dumping data for table `equipment`
+-- Dumping data for table `buildings`
 --
 
-INSERT INTO `equipment` (`name`, `dorm`, `id`, `price`, `date`) VALUES
-('router', 'dorm_f', '1234567890987654321', 123456789, '2012-12-31'),
-('bbs', 'dorm_z', 'wtf', 9999999, '1750-06-01');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `shadow`
---
-
-CREATE TABLE IF NOT EXISTS `shadow` (
-  `no` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `passwd` varchar(40) NOT NULL,
-  `privilege` smallint(6) NOT NULL,
-  PRIMARY KEY (`no`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `shadow`
---
-
-INSERT INTO `shadow` (`no`, `name`, `passwd`, `privilege`) VALUES
-(1, 'kohsiangyu', 'admin', 0),
-(3, 'admin', 'admin', 0),
-(4, 'mkfsn', 'kanade', 0);
+INSERT INTO `buildings` (`index`, `building`, `desc`) VALUES
+(1, 'dorm_a', 1),
+(2, 'dorm_b', 2),
+(3, 'dorm_c', 3),
+(4, 'dorm_d', 4),
+(5, 'dorm_e', 5),
+(6, 'dorm_f', 6),
+(16, 'dorm_52', 11),
+(15, 'dorm_51', 10),
+(14, 'dorm_l', 9),
+(13, 'dorm_h', 8),
+(12, 'dorm_g', 7),
+(17, 'dorm_53', 12),
+(18, 'dorm_54', 13);
