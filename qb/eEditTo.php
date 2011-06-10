6f00c1729b08a592fb64fbe536c41b97c427cae6
mkfsn <p408865@hotmail.com>
2011-06-10 16:36:18 +0900

    編輯器材資訊頁面 字串處理修正完成

--- qb/eEditTo.php
+++ qb/eEditTo.php
@@ -1,6 +1,17 @@
 <?php
 	include("../database/fEdit.php");
-	$return = funcEdit( array("name" => $_POST['addEquivName'], "dorm" => $_POST['addEquivPlace'], "id" => $_POST['addEquivId'], "price" => $_POST['addEquivPrice']) );
+	
+	$EquivName 	= htmlspecialchars ( $_POST['addEquivName']	);
+	$EquivPlace = htmlspecialchars ( $_POST['addEquivPlace']);
+	$EquivId 	= htmlspecialchars ( $_POST['addEquivId']	);
+	$EquivPrice = htmlspecialchars ( $_POST['addEquivPrice']);
+	
+	$EquivName 	= mysql_real_escape_string ( $EquivName  );
+	$EquivPlace = mysql_real_escape_string ( $EquivPlace );
+	$EquivId 	= mysql_real_escape_string ( $EquivId 	);
+	$EquivPrice = mysql_real_escape_string ( $EquivPrice );
+	
+	$return = funcEdit( array("name" => $EquivName, "dorm" => $EquivPlace, "id" => $EquivId, "price" => $EquivPrice) );
 	if(!$return["success"])
 	{
 		echo json_encode($return["status"]);
