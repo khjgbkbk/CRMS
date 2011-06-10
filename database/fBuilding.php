8eef27966ec9bcd8725972eb8707006674a7a456
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 17:10:21 +0800

    remove comments

--- database/fBuilding.php
+++ database/fBuilding.php
@@ -1,21 +1,16 @@
 <?php
 	function funcBuilding($ask=NULL){
-	//	$column_size = 3;
 		include("mysql_connect.php");
 
 		if(!isset($ask['sort'])) $ask['sort'] = "building";
 
 		$sql = "select * from ".$db_building." order by '{$ask['sort']}'";
 		$result = mysql_query($sql) or die(mysql_error());
-		//$row_size = 0;
 
 		while($row = mysql_fetch_row($result)){
 			$data[] = $row;
 		}
-	//	if( count($data) == 0 ){
-	//		return array("success" => false, "status" => "nothing found on database");
-	//	}else{
+
 		return array("success" => true, "data" => $data);
-	//	}
 	}
 ?>
