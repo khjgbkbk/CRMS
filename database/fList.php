913fb3ffc8a43425c46fc2bcba311df78c5d96f8
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 20:24:08 +0800

    join add

--- database/fList.php
+++ database/fList.php
@@ -1,36 +1,23 @@
 <?php
 	function funcList($ask=NULL){
-		$column_size = 5;
 		include("mysql_connect.php");
 
 		if(!isset($ask['sort'])) $ask['sort'] = "name";
 
 		if( isset($ask['dorm']) && $ask['dorm']!="" ){
-			$sql = "select * from ".$db_equip." where dorm = '{$ask['dorm']}' order by {$ask['sort']}";
+			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index where dorm = '{$ask['dorm']}' order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
-			$row_size = 0;
 			while($row = mysql_fetch_row($result)){
-				$data[$row_size] = $row;
-				$row_size++;
+				$data[] = $row;
 			}
-			if( $row_size == 0 ){
-				return array("success" => false);
-			}else{
-				return array("success" => true, "row_size" => $row_size, "column_size" => $column_size, "data" => $data);
-			}
+			return array("success" => true, "data" => $data);
 		}else{
-			$sql = "select * from ".$db_equip." order by {$ask['sort']}";
+			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
-			$row_size = 0;
 			while($row = mysql_fetch_row($result)){
-				$data[$row_size] = $row;
-				$row_size++;
+				$data[] = $row;
 			}
-			if( $row_size == 0 ){
-				return array("success" => false);
-			}else{
-				return array("success" => true, "row_size" => $row_size, "column_size" => $column_size, "data" => $data);
-			}
+			return array("success" => true, "data" => $data);
 		}
 	}
 ?>
