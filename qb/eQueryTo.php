1b559e77918488f0f312fa00c97b80ab7d957cf1
mkfsn <p408865@hotmail.com>
2011-06-10 15:16:23 +0900

    器材查詢頁面之輸入方框 mysql 字串例外處理 完成

--- qb/eQueryTo.php
+++ qb/eQueryTo.php
@@ -1,38 +1,16 @@
 <?php
 	include("../database/fQuery.php");
-	$return = funcQuery( array("id" => $_POST['Id']) );
+	$Id = mysql_real_escape_string ($_POST['Id']);
+	$return = funcQuery( array("id" => $Id) );
 	if(!$return["success"])
 	{
 		echo json_encode("fail");
+		exit;
 	}
 	else
 	{
 		$data = $return['data'];
 		echo json_encode($return['data']);
 		exit;
-		
-		
-		
-		
-		echo json_encode('<table style="border: 5px dotted rgb(109, 2, 107); " align="center" cellPadding="10" frame="border">');
-		echo json_encode('<tbody>');
-		echo json_encode('<tr>');		
-		echo json_encode('<td>名稱</td>');
-		echo json_encode('<td>位置</td>');
-		echo json_encode('<td>編號</td>');
-		echo json_encode('<td>價錢</td>');
-		echo json_encode('<td>加入時間</td>');
-		echo json_encode('</tr>');
-		echo json_encode('<tr>');  
-/*		
-		echo json_encode('<td>'.$data[0].'</td>');
-		echo json_encode('<td>'.$data[1].'</td>');
-		echo json_encode('<td>'.$data[2].'</td>');
-		echo json_encode('<td>'.$data[3].'</td>');
-		echo json_encode('<td>'.$data[4].'</td>');
-		*/
-		echo json_encode('</tr>');
-		echo json_encode('/<tbody>');
-		echo json_encode('</table>');
 	}
 ?>
