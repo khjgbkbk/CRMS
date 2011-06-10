157dba9e0b4506c55f707d4bfb3e1f8c4b718967
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 23:22:42 +0800

    add $data = array();

--- database/fBuilding.php
+++ database/fBuilding.php
@@ -5,6 +5,7 @@
 		if(!isset($ask['sort'])) $ask['sort'] = "building";
 		$sql = "select * from ".$db_building." order by ".$ask['sort'];
 		$result = mysql_query($sql) or die(mysql_error());
+		$data = array();
 		while($row = mysql_fetch_assoc($result)){
 			$data[] = $row;
 		}
