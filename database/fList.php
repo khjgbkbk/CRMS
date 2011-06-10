857c683c356c32c7f91f2a57ed6fafaad1d781d7
mkfsn <p408865@hotmail.com>
2011-06-11 00:20:17 +0900

    補上 array

--- database/fList.php
+++ database/fList.php
@@ -7,6 +7,7 @@
 		if( isset($ask['dorm']) && $ask['dorm']!="" ){
 			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index where dorm = '{$ask['dorm']}' order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
+			$data = array();
 			while($row = mysql_fetch_assoc($result)){
 				$data[] = $row;
 			}
@@ -14,6 +15,7 @@
 		}else{
 			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
+			$data = array();
 			while($row = mysql_fetch_assoc($result)){
 				$data[] = $row;
 			}
