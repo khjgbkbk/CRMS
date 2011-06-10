e0ce3debd88b867ab75a8e7b1f62c304c418d5da
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-10 20:26:30 +0800

    change fetch_row to fetch_assoc

--- database/fList.php
+++ database/fList.php
@@ -7,14 +7,14 @@
 		if( isset($ask['dorm']) && $ask['dorm']!="" ){
 			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index where dorm = '{$ask['dorm']}' order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
-			while($row = mysql_fetch_row($result)){
+			while($row = mysql_fetch_assoc($result)){
 				$data[] = $row;
 			}
 			return array("success" => true, "data" => $data);
 		}else{
 			$sql = "select * from ".$db_equip." left join ".$db_building." on ".$db_equip.".dorm=".$db_building.".index order by {$ask['sort']}";
 			$result = mysql_query($sql) or die(mysql_error());
-			while($row = mysql_fetch_row($result)){
+			while($row = mysql_fetch_assoc($result)){
 				$data[] = $row;
 			}
 			return array("success" => true, "data" => $data);
