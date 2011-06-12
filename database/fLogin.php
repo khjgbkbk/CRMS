29f9a04b837875739a9c1a12926be6b30da7d679
mkfsn <p408865@hotmail.com>
2011-06-12 22:35:57 +0900

    連結資料庫回傳資訊增加

--- database/fLogin.php
+++ database/fLogin.php
@@ -4,20 +4,23 @@
 	*Login Success When ['success'] eq true or eq false
 	*/
 		include("mysql_connect.php");
+		
 		if( isset($user['username']) && isset($user['password']) && $user['username']!="" && $user['password']!="" ){
 			//
 			$sql = "SELECT * FROM ".$db_shadow." where name = '{$user['username']}'";
-			$result = mysql_query($sql) or die(mysql_error());
+			$result = mysql_query($sql);
+			if(!$result)
+				return array("success" => false, "status"=>"Connect failed");
 			$row = mysql_fetch_row($result);
 
 			if( $row[1]==$user['username'] && $row[2]==$user['password'] ){
 				return array("success" => true);
 			}else{
-				return array("success" => false);
+				return array("success" => false, "status"=>"nosuchaccount");
 			}
 		}else{
 			//
-			return array("success" => false);
+			return array("success" => false, "status"=>"???");
 		}
 	}
 ?>
