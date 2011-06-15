f271d18f8e421296cab9ef6ae1a1a7ef05b14d6f
Ko Hsiang Yu <kohsiangyu@gmail.com>
2011-06-15 23:01:14 +0800

    Change the creation of default id to time()%86400

--- database/fNew.php
+++ database/fNew.php
@@ -5,7 +5,7 @@
 		if(isset($ask)){
 			if(!isset($ask['name'])) $ask['name'] = "UnnamedEquipment";
 			if(!isset($ask['dorm'])) $ask['dorm'] = "?";
-			if(!isset($ask['id']) || $ask['id']=="") $ask['id'] = date("ymd").sprintf("%05d",time()%100000);
+			if(!isset($ask['id']) || $ask['id']=="") $ask['id'] = date("ymd").sprintf("%05d",time()%86400);
 			if(!isset($ask['price'])) $ask['price'] = 0;
 //isset($ask['name']) && isset($ask['dorm']) && isset($ask['id']) && isset($ask['price']) && $ask['name']!="" && $ask['dorm']!="" && $ask['id']!="" && $ask['price']!="" ){
 			$sql = "select * from ".$db_equip." where id = '{$ask['id']}'";
