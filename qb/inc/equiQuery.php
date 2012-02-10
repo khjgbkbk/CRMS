4e93aa1412755bcc2a53733a2ee7f50f9ae1b1fa
PichuChen <pichubaby@gmail.com>
2012-02-11 00:34:23 +0800

    build Query equipment

--- qb/inc/equiQuery.php
+++ qb/inc/equiQuery.php
@@ -2,48 +2,7 @@
 function showDataMsg(){
 ?>
 
-<script type="text/javascript">
-	/*function send()
-	{
-		if( $("input[name='Equivid']").attr('value') == "" )
-		{
-			$('div#message').html('請輸入編號!');
-			return;
-		}
-		$.ajax({
-			url: 'eQueryTo.php',
-			type: 'POST',
-			data: {
-				Id: 	$("input[name='Equivid']").attr('value'),
-			},
-			dataType: "json",
-			error: function(xhr) {
-				alert('Ajax request failure');
-			},
-			success: function(result) {
-				switch (result) {
-				case "fail":
-					$('div#message').html('Not found');
-					break;
-				default:
-					$('div#message').html('<table style="border: 5px dotted rgb(109, 2, 107); " align="center" cellPadding="10" frame="border"><tbody><tr>		<td>名稱</td><td>位置</td><td>編號</td><td>價錢</td><td>加入時間</td></tr><tr>  <td>'+result["name"]+'</td><td>'+result["building"]+'</td><td>'+result["id"]+'</td><td>'+result["price"]+'</td><td>'+result["date"]+'</td></tr></tbody></table>');
-					break;
-				}
-			}
-		});
-	}*/
-</script>
-
-<script type="text/javascript">
-	/*var KEY_ENTER = 13;
-	$(document).ready(function () 
-	{
-		$('#Send').click(function()
-		{
-			send();
-		});
-	});*/
-</script>
+<script type="text/javascript"><?php include('inc/js/equiQuery.js'); ?></script>
 
 查詢器材
 <br><br>
