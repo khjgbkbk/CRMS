d35b35f1b09ef556c98ea793d3e0d0dbf7c3fefb
PichuChen <pichubaby@gmail.com>
2012-02-11 12:23:00 +0800

    debug

--- database/fGetQRCode.php
+++ database/fGetQRCode.php
@@ -5,13 +5,19 @@ function funcGetQRCode($ask){
 	if($res['success'] == false){
 		return array("success" => false);
 	}else{
-		$Code = array(
-			'version' => 1,
-			'id'      => $res["data"]['id'],
-			'name'	  => $res["data"]['name']
-		);
-		$uri = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&choe=UTF-8&chld=h&chl=' . json_encode($Code);
+		$Code = funcMkQRCodeData($res);
+		$uri = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&choe=UTF-8&chld=h&chl=' . urlencode($Code['data']);
 		return array("success" => true, "data" => $uri);
 	}
 }
+function funcMkQRCodeData($data){
+	$ret = array(
+		'version' => 1,
+		'id'      => $data['id'],
+		'name'	  => $data['name']
+	);
+	return array('success' => true , 'data' => $json_encode(ret));
+}
+
+
 ?>
