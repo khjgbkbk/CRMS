eee87ba62dfac57b9dc5a3334b2861270472daf8
PichuChen <pichubaby@gmail.com>
2012-03-16 00:07:53 +0800

    direct fetch data

--- qb/inc/js/equiQuery.js
+++ qb/inc/js/equiQuery.js
@@ -11,8 +11,9 @@ function send()
 		dataType: "json",
 		statusCode:{
 			200:function(result){
+			/*
 				$('div#message').html('<table style="border: 5px dotted rgb(109, 2, 107); " align="center" cellPadding="10" frame="border"><tbody><tr>		<td>名稱</td><td>位置</td><td>編號</td><td>價錢</td><td>加入時間</td></tr><tr>  <td>'+result["name"]+'</td><td>'+result["building"]+'</td><td>'+result["id"]+'</td><td>'+result["price"]+'</td><td>'+result["date"]+'</td></tr></tbody></table>');
-			
+			*/
 			
 			}
 		
